34deca0069f6b1e57fe81bf0b5256844e1b51fc7
gensichen <gensichen@gmail.com>
2025-07-05 12:51:56 +0200

    Add Parts controller.

---
+++ src/PartsTrackerApi/PartsTrackerApi/Controllers/PartsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PartsTrackerApi.Data;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PartsController : ControllerBase
+{
+    private readonly PartsDbContext _context;
+
+    public PartsController(PartsDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/parts
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Part>>> GetParts()
+    {
+        var parts = await _context.Parts.ToListAsync();
+        return Ok(parts);
+    }
+}
