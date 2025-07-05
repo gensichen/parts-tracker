5a390f0343ee3c087f4bf6f40b3f2a431b0cc94e
gensichen <gensichen@gmail.com>
2025-07-05 12:52:55 +0200

    Get the database going.

---
+++ src/PartsTrackerApi/PartsTrackerApi/Data/PartsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PartsTrackerApi.Data;
+
+public class PartsDbContext : DbContext
+{
+    public DbSet<Part> Parts { get; set; }
+
+    public PartsDbContext(DbContextOptions<PartsDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Part>().HasKey(p => p.PartNumber);
+        
+        // Optional: Seed initial data
+        modelBuilder.Entity<Part>().HasData(
+            new Part { PartNumber = "MB1001", Description = "Bolt", QuantityOnHand = 60, LocationCode = "FP1", LastStockTake = new DateTime(2024, 01, 01, 0, 0, 0, DateTimeKind.Utc) },
+            new Part { PartNumber = "MB1002", Description = "Nut", QuantityOnHand = 100, LocationCode = "FP2", LastStockTake = new DateTime(2024, 01, 01, 0, 0, 0, DateTimeKind.Utc) },
+            new Part { PartNumber = "MB1003", Description = "Washer", QuantityOnHand = 75, LocationCode = "FP2", LastStockTake = new DateTime(2024, 01, 01, 0, 0, 0, DateTimeKind.Utc) }
+        );
+    }
+}
--- src/PartsTrackerApi/PartsTrackerApi/Program.cs
+++ src/PartsTrackerApi/PartsTrackerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PartsTrackerApi.Data;
+
 namespace PartsTrackerApi;
 
 public class Program
@@ -7,6 +10,9 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddDbContext<PartsDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
