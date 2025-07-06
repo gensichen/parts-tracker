2a6315961cd031d4a90bdd69129220e6531d6d4e
gensichen <gensichen@gmail.com>
2025-07-07 00:20:08 +0200

    Update edit button to use the part number correctly

--- src/PartsTrackerUI/src/components/buttons/EditPartButton.jsx
+++ src/PartsTrackerUI/src/components/buttons/EditPartButton.jsx
@@ -2,10 +2,9 @@ import { Button } from 'antd'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 const EditPartButton = ({part}) => {
     return (
-        <Link to={`/parts/edit/${part}`}>
+        <Link to={`/parts/edit/${part.partNumber}`}>
             <Button type='primary'>
                 Edit
             </Button>
